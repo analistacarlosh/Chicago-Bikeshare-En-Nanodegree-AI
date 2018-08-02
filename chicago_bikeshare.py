f3f5c6cf28a331ca44e70e92e06f3b2d9e498764
Carlos Fructuoso <analista.carlosh@gmail.com>
2018-08-02 13:56:01 -0300

    Task 10 - Done

--- chicago_bikeshare.py
+++ chicago_bikeshare.py
@@ -206,21 +206,22 @@ assert round(mean_trip) == 940, "TASK 9: mean_trip with wrong result!"
 assert round(median_trip) == 670, "TASK 9: median_trip with wrong result!"
 # -----------------------------------------------------
 
-'''
 input("Press Enter to continue...")
 # TASK 10
 # Gender is easy because usually only have a few options. How about start_stations? How many options does it have?
 # TODO: Check types how many start_stations do we have using set()
-user_types = set()
+start_stations = column_to_list(data_list, 3)
+user_types = set(start_stations)
 
 print("\nTASK 10: Printing start stations:")
 print(len(user_types))
-print(user_types)
+# print(user_types)
 
 # ------------ DO NOT CHANGE ANY CODE HERE ------------
 assert len(user_types) == 582, "TASK 10: Wrong len of start stations."
 # -----------------------------------------------------
 
+'''
 input("Press Enter to continue...")
 # TASK 11
 # Go back and make sure you documented your functions. Explain the input, output and what it do. Example:
